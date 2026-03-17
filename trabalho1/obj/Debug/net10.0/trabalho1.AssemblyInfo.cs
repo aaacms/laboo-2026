6c1fb572582a939ca1173fabef81abf4faf55da2
Amanda Siebeneichler <amanda.sieb10@gmail.com>
2026-03-17 14:44:08 -0300

    alteracoes

--- trabalho1/obj/Debug/net10.0/trabalho1.AssemblyInfo.cs
+++ trabalho1/obj/Debug/net10.0/trabalho1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("trabalho1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95b3f201f5d62132f2d32af6d18e650fd53772b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("trabalho1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("trabalho1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
