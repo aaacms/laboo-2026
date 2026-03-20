f7f0a49c255054e54b3c407d683a94dc11259447
Amanda Siebeneichler <amanda.sieb10@gmail.com>
2026-03-20 17:13:38 -0300

    mudancas

--- trabalho1/projeto/Program.cs
+++ trabalho1/projeto/Program.cs
@@ -1,2 +1,2 @@
-﻿var db = new DatabaseConnection();
-db.Conectar();
+﻿var db = new DatabaseConnection("Host=localhost;Port=10008;Username=postgres;Password=aaacms;Database=laboo");
+var conn = db.GetConnection();
