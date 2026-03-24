dbc2b092f824be179550e3005713079d2c740bac
Amanda Siebeneichler <amanda.sieb10@gmail.com>
2026-03-24 13:45:14 -0300

    configuracao do appsettings.json

--- trabalho1/projeto/Program.cs
+++ trabalho1/projeto/Program.cs
@@ -1,2 +1,14 @@
-﻿var db = new DatabaseConnection("Host=localhost;Port=10008;Username=postgres;Password=aaacms;Database=laboo");
-var conn = db.GetConnection();
+﻿using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("projeto/appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+string host = config["Database:Host"] ?? throw new Exception("Database:Host não foi definido.");
+string port = config["Database:Port"] ?? throw new Exception("Database:Port não foi definido.");
+string username = config["Database:Username"] ?? throw new Exception("Database:Username não foi definido.");
+string password = config["Database:Password"] ?? throw new Exception("Database:Password não foi definido.");
+string database = config["Database:Name"] ?? throw new Exception("Database:Name não foi definido.");
+
+var db = new DatabaseConnection($"Host={host};Port={port};Username={username};Password={password};Database={database}");
