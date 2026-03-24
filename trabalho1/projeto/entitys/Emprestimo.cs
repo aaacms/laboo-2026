995ced7f9454a8d07acd637d8eab7cc06a7f1d22
Gabriel Saueressig <gabsauere@gmail.com>
2026-03-24 14:48:49 -0300

    Entidades ajeitadas

--- trabalho1/projeto/entitys/Emprestimo.cs
+++ trabalho1/projeto/entitys/Emprestimo.cs
@@ -1,7 +1,7 @@
 public class Emprestimo
 {
     public int Id { get; set; }
-    public int UsuarioId { get; set; }
-    public int LivroId { get; set; }
+    public int Usuario_Id { get; set; }
+    public int Livro_Id { get; set; }
     public DateTime Data_emprestimo { get; set; }
 }
--- trabalho1/projeto/entitys/Livro.cs
+++ trabalho1/projeto/entitys/Livro.cs
@@ -2,5 +2,5 @@ public class Livro
 {
     public int Id { get; set; }
     public string Titulo { get; set; }
-    public string AutorId { get; set; }
+    public int Autor_Id { get; set; }
 }
