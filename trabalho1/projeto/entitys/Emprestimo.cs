5834a880cd2e536bacfffece748a2d1aeb6abef5
Gabriel Saueressig <gabsauere@gmail.com>
2026-03-24 14:46:40 -0300

    Entidades Feitas

---
+++ trabalho1/projeto/entitys/Emprestimo.cs
@@ -0,0 +1,7 @@
+public class Emprestimo
+{
+    public int Id { get; set; }
+    public int UsuarioId { get; set; }
+    public int LivroId { get; set; }
+    public DateTime Data_emprestimo { get; set; }
+}
---
+++ trabalho1/projeto/entitys/Livro.cs
@@ -0,0 +1,6 @@
+public class Livro
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string AutorId { get; set; }
+}
